b7da307d4bd87593401e3b2844e8ff9c1f416891
William Parson <>
2021-04-27 15:35:54 +0000

    spelling errors lol

--- commands/dailyget.py
+++ commands/dailyget.py
@@ -4,8 +4,8 @@ from datetime import date
 
 today = date.today()
 if today.strftime("%m/%d/%y") == currency.getDailyDate(arguments.currentmessage.author.id):
-  arguments.messageReturn = "You have already claimed you dialy for today"
+  arguments.messageReturn = "You have already claimed your daily for today."
 else:
   currency.addCurr(arguments.currentmessage.author.id, 50)
   currency.changeDailyDate(arguments.currentmessage.author.id)
-  arguments.messageReturn = "You have claimed you daily, you total is now %s" % (currency.getCurr(arguments.currentmessage.author.id))
+  arguments.messageReturn = "You have claimed your daily, you total is now %s" % (currency.getCurr(arguments.currentmessage.author.id))
